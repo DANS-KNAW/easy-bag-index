4576bc555fc45686a69763f2cc2e8a317a9453d5
Jan van Mansum <janvanmansum@gmail.com>
2024-02-05 11:23:50 +0100

    Removed tests that are causing problems with Java 8.

--- src/test/resources/database/bag-index.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE bag_info (
-    bagId CHAR(36) NOT NULL PRIMARY KEY,
-    base CHAR(36) NOT NULL,
-    created CHAR(29) NOT NULL,
-    doi VARCHAR(256),
-    urn VARCHAR(256) NOT NULL
-);
